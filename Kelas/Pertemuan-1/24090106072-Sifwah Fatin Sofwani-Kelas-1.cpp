927c94ac79ef133e7bb2990ebebc3c8ac80f608c
SifwahFatinSofwani <sifwahfsofwani@gmai.com>
2025-03-09 17:28:27 +0800

    coding kelas

---
+++ Kelas/Pertemuan-1/24090106072-Sifwah Fatin Sofwani-Kelas-1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    // cout << "Belajar C++ sangat seru😀, kali";
+
+    // string nama = "Sifwah gagah";
+    // int umur = 18;
+    // cout << "Halo, nama saya " << nama << " dan umur saya " << umur << " tahun";
+
+    // string nama;
+    // cout << "Masukkan nama koe: ";
+    // getline(cin, nama);
+    // cout << "Halo, " << nama << ", selamat datang!" << endl;
+
+    // int a;
+    // cout << "Masukkan nilai a: ";
+    // cin >> a;
+    // cout << "masukkan nilai b: ";
+    // int b;
+    // cin >> b;
+    // cout << "hasil a + b = " << a + b << endl;
+
+    // int a, b;
+    // cout << "Masukkan nilai a: ";
+
+    // cin >> a;
+    // cout << "masukkan nilai b: ";
+
+    // cin >> b;
+    // if (a > b) {
+    //     cout << "a lebih besar dari b" << endl;
+    // } else if (a < b) {
+    //     cout << "a lebih kecil dari b" << endl;
+    // } else {
+    //     cout << "a sama dengan b" << endl;
+    // }
+
+    // bool sudahganteng = true;
+    // string status = sudahganteng ? "Sudah ganteng" : "Belum ganteng";
+    // cout << status << endl;
+
+    // int i = 0;
+    // while (i < 5) {
+    // cout << i << endl;s
+    // i++;
+    // }
+
+    // int arr[] = {10,12,23,23,4};
+    // for (int x : arr) {
+    // cout << x << endl;
+    // }
+    // return 0;
+
+    // cout << "Masukkan nilai: ";
+    // int nilai;
+    // cin >> nilai;
+    // switch (nilai) {
+    //     case 10:
+    //         cout << "Nilai maksimal" << endl;
+    //         break;
+    //     case 7:
+    //         cout << "Nilai cukup" << endl;
+    //         break;
+    //     case 4:
+    //         cout << "Nilai kurang" << endl;
+    //         break;
+    //     default:
+    //         cout << "Nilai tidak valid" << endl;
+    // }
+}
