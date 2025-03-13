1077c70dc7894666ce3d4986c64838353491dc83
SifwahFatinSofwani <sifwahfsofwani@gmai.com>
2025-03-13 10:40:26 +0800

    coding kelas

---
+++ Kelas/Pertemuan-2/24090106072-Sifwah Fatin Sofwani-Kelas-2.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+#define max 100
+int panjang = 0; 
+int arr[max];
+string pilihan;
+int index;
+
+int main () {
+    system ("cls");
+
+    // string nama []= {"aldi", "rio", "nuga", "dedi", "edi"};
+
+    // cout << "Total nama: " << sizeof(nama) / sizeof(nama[0]) << endl;
+    // for(int i = 0; i < sizeof(nama) / sizeof(nama[0]); i++) {
+    //     cout << nama[i] << endl;
+    // }
+
+    // cout << sizeof(nama) / sizeof(string) << endl;
+
+    // for (string x : nama) {
+    //     cout << x << endl;
+    // }
+
+    // string nama [4][4] = {
+    //     {"aldi", "rio", "nuga", "dedi"},
+    //     {"edi", "dzaki", "nugroho", "negro"},
+    //     {"alif", "davin", "khakim", "irahm"},
+    //     {"valen", "chou", "suyou", "Yss"}
+    // };
+
+    // cout << endl;
+    // int panjangnama = sizeof(nama) / sizeof(nama[0][0]);
+    // cout << panjangnama << endl;
+    // cout << endl;
+
+
+    // cout << "nama [0][1]: " << nama[0][1] << endl;
+    // cout << endl;
+
+    // cout << "Total nama: " << sizeof(nama) / sizeof(nama[0]) << endl;
+    // for(int i = 0; i < sizeof(nama) / sizeof(nama[0]); i++) {
+    //     for(int j = 0; j < sizeof(nama[0]) / sizeof(string); j++) {
+    //         cout << nama[i][j] << endl;
+    //     }
+    //     cout << endl;
+    // }
+
+    // int arr[3][3][3] = {
+    // {
+    // {1, 2, 3},
+    // {4, 5, 6},
+    // {7, 8, 9}
+    // },
+    // {
+    // {10, 11, 12},
+    // {13, 14, 15},
+    // {16, 17, 18}
+    // },
+    // {
+    // {19, 20, 21},
+    // {22, 23, 24},
+    // {25, 26, 27}
+    // }
+    // };
+    // cout << arr[1][2][1] << endl; // Output: 17
+
+    // return 0;
+
+    while (true) {
+        cout << "Pilih menu" << endl;
+        cout << "1. Tambah data" << endl;
+        cout << "2. Hapus data" << endl;
+        cout << "3. Tampilkan data" << endl;
+        cout << "4. Keluar" << endl;
+        cout << "Pilihan: ";
+        getline (cin, pilihan);
+        
+        
+    }       
+
+
+    
+}
